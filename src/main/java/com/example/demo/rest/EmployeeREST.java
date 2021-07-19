0eeae4562ffed1767974ae2ed10a619b75db0439
irarevalo <ivan2397@hotmail.es>
2021-07-18 20:56:57 -0500

    Add files via upload
    Modificación CORS

--- src/main/java/com/example/demo/rest/EmployeeREST.java
+++ src/main/java/com/example/demo/rest/EmployeeREST.java
@@ -7,11 +7,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
@@ -24,6 +26,7 @@ public class EmployeeREST {
 	@Autowired
 	private EmployeeService employeeService;
 	
+	@CrossOrigin
 	@PostMapping
 	private ResponseEntity<Employee> guardar (@RequestBody Employee employee){
 		Employee temp = employeeService.create(employee);
@@ -34,11 +37,13 @@ public class EmployeeREST {
 		}
 	}
 	
+	@CrossOrigin
 	@GetMapping
 	private ResponseEntity<List<Employee>> ListAll (){
 		return ResponseEntity.ok(employeeService.getAllEmployees());
 	}
 	
+	@CrossOrigin
 	@GetMapping (value="{id}")
 	private ResponseEntity<Optional<Employee>> ListById (@PathVariable ("id") Long id){
 		return ResponseEntity.ok(employeeService.findById(id));
